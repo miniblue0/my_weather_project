611dae1d7db5ff2e070454d415a05943c27cb279
miniblue0 <agustinezequielnievas69@gmail.com>
2024-11-27 19:49:39 -0300

    Update CRT_weatherData.sql

--- querysSQL/CRT_weatherData.sql
+++ querysSQL/CRT_weatherData.sql
@@ -1,11 +1,30 @@
-CREATE DATABASE weather_db;
+--CREATE DATABASE weather_db;
 
+USE [weather_db]
+GO
 
-CREATE TABLE WeatherData (
-    id INT PRIMARY KEY IDENTITY(1,1),
-    city_name NVARCHAR(50),
-    temperature FLOAT,
-    humidity INT,
-    weather_description NVARCHAR(255),
-    date_time DATETIME
-);
+/****** Object:  Table [dbo].[WeatherData]    Script Date: 27/11/2024 19:12:17 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[WeatherData](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[city_name] [varchar](100) NOT NULL,
+	[country] [varchar](100) NOT NULL,
+	[temperature] [float] NOT NULL,
+	[minimum temperature] [float] NOT NULL,
+	[maximum temperature] [float] NOT NULL,
+	[humidity] [int] NOT NULL,
+	[weather_description] [varchar](255) NOT NULL,
+	[date_time] [datetime] NOT NULL,
+ CONSTRAINT [unique_city_datetime] UNIQUE NONCLUSTERED 
+(
+	[city_name] ASC,
+	[date_time] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
