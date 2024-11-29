cf9ede92f589f651a17c57ff5b4c3454bc1b1477
miniblue0 <agustinezequielnievas69@gmail.com>
2024-11-29 01:22:32 -0300

    Update CRT_weatherData.sql

--- querysSQL/CRT_weatherData.sql
+++ querysSQL/CRT_weatherData.sql
@@ -1,9 +1,7 @@
---CREATE DATABASE weather_db;
-
 USE [weather_db]
 GO
 
-/****** Object:  Table [dbo].[WeatherData]    Script Date: 27/11/2024 19:12:17 ******/
+/****** Object:  Table [dbo].[WeatherData]    Script Date: 29/11/2024 01:21:38 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -15,11 +13,13 @@ CREATE TABLE [dbo].[WeatherData](
 	[city_name] [varchar](100) NOT NULL,
 	[country] [varchar](100) NOT NULL,
 	[temperature] [float] NOT NULL,
-	[minimum temperature] [float] NOT NULL,
-	[maximum temperature] [float] NOT NULL,
-	[humidity] [int] NOT NULL,
 	[weather_description] [varchar](255) NOT NULL,
 	[date_time] [datetime] NOT NULL,
+ CONSTRAINT [UC_CityDate] UNIQUE NONCLUSTERED 
+(
+	[city_name] ASC,
+	[date_time] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
  CONSTRAINT [unique_city_datetime] UNIQUE NONCLUSTERED 
 (
 	[city_name] ASC,
